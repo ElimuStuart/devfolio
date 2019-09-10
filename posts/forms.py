eb73eb1d2a9b5b7f9e253c7f0fb8f28eb74d10f4
ElimuStuart <stuartelimu@gmail.com>
2019-09-10 10:36:29 +0300

    create EmailPost form

--- posts/forms.py
+++ posts/forms.py
@@ -7,6 +7,11 @@ class TinyMCEWidget(TinyMCE):
     def use_required_attribute(self, *args):
         return False
 
+class EmailPostForm(forms.Form):
+    name = forms.CharField(max_length=25)
+    email = forms.EmailField()
+    to = forms.EmailField()
+    comments = forms.CharField(required=False, widget=forms.Textarea)
 
 class PostForm(forms.ModelForm):
     content = forms.CharField(
