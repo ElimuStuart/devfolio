9895920743900d45bd0eb6bdd61c308f8ddd8eed
ElimuStuart <stuartelimu@gmail.com>
2019-09-10 14:11:18 +0300

    create sitemap to the website

--- blog/urls.py
+++ blog/urls.py
@@ -19,11 +19,18 @@ from django.urls import path, include
 from django.conf.urls.static import static
 from filebrowser.sites import site
 
+from django.contrib.sitemaps.views import sitemap
+from posts.sitemaps import PostSitemap
+
+sitemaps = {
+    'posts': PostSitemap,
+}
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('tinymce/', include('tinymce.urls')),
     path('', include('posts.urls', namespace='posts')),
+    path('sitemap.xml', sitemap, {'sitemaps':sitemaps},name='django.contrib.sitemaps.views.sitemap'),
     
 ]
 
