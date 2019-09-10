e2e14b9ccdde96c0b138cd66a0cf82a30c517a04
ElimuStuart <stuartelimu@gmail.com>
2019-09-10 10:14:25 +0300

    add pagination to blog.html

--- posts/views.py
+++ posts/views.py
@@ -74,8 +74,18 @@ def index(request):
     return render(request, 'index.html', context)
 
 def post_list(request):
-    posts = Post.published.all()
-    return render(request, 'blog.html', {'posts': posts})
+    object_list = Post.published.all()
+    paginator = Paginator(object_list, 3)
+    page = request.GET.get('page')
+    try:
+        posts = paginator.page(page)
+    except PageNotAnInteger:
+        # if page is not an integer deliver the first page
+        posts = paginator.page(1)
+    except EmptyPage:
+        # if page is out of range deliver the last page
+        posts = paginator.page(paginator.num_pages)
+    return render(request, 'blog.html', {'posts': posts, 'page':page})
 
 def post_detail(request, year, month, day, post):
     post = get_object_or_404(
